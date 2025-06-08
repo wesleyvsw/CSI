6096e9e0c39901efd711c5032a01e8bd90e74e81
wesleyvsw <64928214+wesleyvsw@users.noreply.github.com>
2025-06-08 11:04:49 -0300

    Add files via upload

---
+++ trab2_csi.m
@@ -0,0 +1,24 @@
+%Regulação Primaria
+%Definindo Areas
+D1 =1;
+R1=1;
+r = 1;
+Tt = 1;
+Tg = 1;
+T1 = 1;
+Tw =1;
+Ts1 =1;
+T11 = 1;
+K12 =1;
+B1 =1;
+tau1 =1;
+%Turbina a gás
+D2 =1;
+R2 = 1;
+T12 = 1;
+T2 = 1;
+Ts2 = 1;
+Tf = 1;
+Ts = 1;
+B2 =1;
+tau2 =1;
